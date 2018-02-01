297967602464e432b6cbab5f5dde160d1ee5f2ae
Lauren Light <laurenmlight@ufl.edu>
2018-02-01 17:30:38 -0500

    add president array

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 var iceCream = ['mint chocolate chip', 'chocolate chip cookie dough', 'chocolate',
-	'vanilla'];
+    'vanilla'
+];
+
+var presidents = [
+    {firstName: 'Donald', lastName: 'Trump', termLength: 4, party: 'Republican', yearsOfPresidency: 1},
+    {firstName: 'Barack', lastName: 'Obama', termLength: 8, party: 'Democrat', yearsOfPresidency: 8},
+    {firstName: 'George W.', lastName: 'Bush', termLength: 8, party: 'Republican', yearsOfPresidency: 8},
+    {firstName: 'Bill', lastName: 'Clinton', termLength: 8, party: 'Democrat', yearsOfPresidency: 8},
+    {firstName: 'George', lastName: 'Bush', termLength: 4, party: 'Republican', yearsOfPresidency: 4}
+];
+
+console.log(presidents[2].firstName, presidents[2].lastName);
