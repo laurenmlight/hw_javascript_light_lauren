aa9d4bbf3b73287ac0071a38d4281e19de04000c
Lauren Light <laurenmlight@ufl.edu>
2018-02-04 16:24:43 -0500

    add functions to project

--- js/main.js
+++ js/main.js
@@ -11,3 +11,30 @@ var presidents = [
 ];
 
 console.log(presidents[2].firstName, presidents[2].lastName);
+
+function howdy(person, message) {
+    return person + ' says ' + message;
+}
+
+console.log(howdy('Lauren Light', 'hello!'));
+
+var numba = function (string) {
+    if (string.length < 7) {
+        console.log('What a short little word!');
+    }
+    else if (string.length > 7) {
+        console.log("I'm sorry, but that's too many to count.");
+    }
+    else if (string.length === 7) {
+        console.log('7, what a perfect choice!');
+    }
+};
+numba('7');
+numba('seventy');
+numba('university');
+
+function inception(display, favMovie) {
+    return favMovie + display;
+}
+
+console.log(inception(' is a fantastic movie.', 'Harry Potter'));
